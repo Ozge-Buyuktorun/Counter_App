b12bcbd4eecc75480010ebf5ea6af3f50caa2f80
Özge Büyüktorun <74399824+Ozge-Buyuktorun@users.noreply.github.com>
2023-03-01 09:47:27 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,113 +3,51 @@
 import 'package:flutter/material.dart';
 void main() { runApp(const MyApp());}
 class MyApp extends StatelessWidget {
+
+Container containerOlustur(String harf,Color color, {double margin = 0}) {
+  return Container(
+        width: 75,
+        height: 75,
+        alignment: Alignment.center,
+        color: color,
+        child: Text(harf, style: const TextStyle(fontSize: 48, color: Colors.black,),
+        ),
+        );
+}
 Row dartRowOlustur() {
   return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-      Container(   
-        width: 75,
-        height: 75,
-        alignment: Alignment.center,
-        color: Colors.green.shade800,
-        child: const Text('D', style: TextStyle(fontSize: 48, color: Colors.black,))),
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+       children: [
+      Container( 
+        child: containerOlustur( 'D' , Colors.green.shade200 ),),
       Container(    
-          width: 75,
-          height: 75,
-          alignment: Alignment.center,
-          color: Colors.orange.shade200,
-         child: const Text('A', style: TextStyle(fontSize: 48, color: Colors.black,))),
+          child: containerOlustur( 'A' , Colors.green.shade600 ),),
       Container(
-          width: 75,
-          height: 75,
-          alignment: Alignment.center,
-          color: Colors.orange.shade300,
-          child: const Text('R', style: TextStyle(fontSize: 48, color: Colors.black,))),
+          child: containerOlustur( 'R' , Colors.green.shade600 ),),
       Container(
-        width: 75,
-        height: 75,
-        alignment: Alignment.center,
-        color: Colors.orange.shade400,
-        child: const Text('T', style: TextStyle(fontSize: 48, color: Colors.black,))),
+        child: containerOlustur( 'T' , Colors.green.shade800 ),),
     ],);
   }
     Column dersleriColumnOlustur() {
           return Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(child: Container(
-              width: 75,
-              height: 75,
-              alignment: Alignment.center,
-              color: Colors.green.shade800,
-              margin: EdgeInsets.only(top: 15),
-              child: const Text('E', style: TextStyle(fontSize: 48),
-              ),
-            ),
-            ),
-          Expanded(child: Container(
-              width: 75,
-              height: 75,
-              alignment: Alignment.center,
-              margin: EdgeInsets.only(top: 15),
-              color: Colors.green.shade800,
-              child: const Text('R', style: TextStyle(fontSize: 48),
-              ),
-            ),
-            ),
-            Expanded(child: Container(
-              width: 75,
-              height: 75,
-              alignment: Alignment.center,
-              color: Colors.green.shade800,
-              margin: EdgeInsets.only(top: 15),
-              child: const Text('S', style: TextStyle(fontSize: 48),
-              ),
-            ),
-            ),
-            Expanded(child: Container(
-              width: 75,
-              height: 75,
-              alignment: Alignment.center,
-              color: Colors.green.shade800,
-              margin: EdgeInsets.only(top: 15),
-              child: const Text('L', style: TextStyle(fontSize: 48),
-              ),
-            ),
-            ), 
-            Expanded(child: Container(
-              width: 75,
-              height: 75,
-              alignment: Alignment.center,
-              color: Colors.green.shade800,
-              margin: EdgeInsets.only(top: 15),
-              child: const Text('E', style: TextStyle(fontSize: 48),
-              ),
-            ),
-            ),
-            Expanded(child: Container(
-              width: 75,
-              height: 75,
-              alignment: Alignment.center,
-              color: Colors.green.shade800,
-              margin: EdgeInsets.only(top: 15),
-              child: const Text('R', style: TextStyle(fontSize: 48),
-              ),
-            ),
-            ),
-            Expanded(child: Container(
-              width: 75,
-              height: 75,
-              alignment: Alignment.center,
-              color: Colors.green.shade800,
-              margin: EdgeInsets.only(top: 15),
-              child: const Text('I', style: TextStyle(fontSize: 48),
-              ),
-            ),
-            ),
-          ],
-        );
-    }
+            Expanded(
+               child: containerOlustur('E', Colors.green.shade400, margin: 15),),
+            Expanded(
+              child: containerOlustur('R', Colors.green.shade600, margin: 15),),
+            Expanded(
+              child: containerOlustur('S', Colors.green.shade800, margin: 15),),
+            Expanded(
+              child: containerOlustur('L', Colors.green.shade600, margin: 15),),
+            Expanded(
+              child: containerOlustur('E', Colors.green.shade400, margin: 15),),
+            Expanded(
+              child: containerOlustur('R', Colors.green.shade200, margin: 15),),
+                 Expanded(
+              child: containerOlustur('I', Colors.green.shade200, margin: 15),),
+          ],);
+          }
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
@@ -137,44 +75,3 @@ Row dartRowOlustur() {
     );
   }
 }
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// void main() {
-//   runApp(const MyApp());
-// }
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp( 
-//       debugShowCheckedModeBanner: false,
-//       theme : ThemeData( primaryColor: Colors.greenAccent),
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Header', style: TextStyle(fontWeight: FontWeight.bold), selectionColor: 
-//            Colors.black), centerTitle:    
-//            true, backgroundColor: Colors.deepOrangeAccent,  
-//         ),
-//         body : Container( color: Colors.indigo,
-//          height: 600,
-//           child: Column(
-//             mainAxisSize: MainAxisSize.max,
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             crossAxisAlignment: CrossAxisAlignment.stretch,
-//             children: const <Widget>[
-//               Icon( Icons.add_circle, size: 64, color: Colors.green),
-//               Icon( Icons.add_circle, size:64, color: Colors.purpleAccent),
-//               Icon ( Icons.add_circle_rounded, size: 64, color: Colors.pinkAccent),
-//               Icon( Icons.add_circle_sharp, size : 68, color: Colors.yellowAccent),
-//               Icon( Icons.add_circle_sharp, size : 68, color: Colors.blueAccent),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
